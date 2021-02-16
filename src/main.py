ad475bf2eb6ea8445add321b57195f33f0e45ace
mohdakibuddin <mohdakibuddin@gmail.com>
2021-02-15 23:09:37 -0500

    fix: corrected typo on line 7 "minimum" to "maximum"

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from rng import generateRandomNumber
 def main():
     print("This program generates a random number between a minimum and maximum")
     minimum = int(input("Enter a minimum number, then hit enter: "))
-    maximum = int(input("Enter a minimum number, then hit enter: "))
+    maximum = int(input("Enter a maximum number, then hit enter: "))
     randomNumber = generateRandomNumber(minimum, maximum)
     print(randomNumber)
 
